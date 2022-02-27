6ddd4f2fe891095af0c078e3cc3cdeb89bef3c98
Javier <mscjaviergs@gmail.com>
2022-02-27 04:45:28 -0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,6 @@ public class App {
     System.out.println("I am your awesome Java application!");
     Delegate delegate = new Delegate();
     int result = delegate.answer();
-    System.out.println(ans);
+    System.out.println(result);
   }
 }
