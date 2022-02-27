23fb09f63b15b615b898084752c250ce7672fe4d
Javier <mscjaviergs@gmail.com>
2022-02-27 03:47:25 -0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+
+  public static void main( String[] args ) {
+  
+    System.out.println("I am your awesome Java application!");
+    Delegate delegate = new Delegate();
+    int result = delegate.answer();
+    System.out.println(ans);
+  }
+}
