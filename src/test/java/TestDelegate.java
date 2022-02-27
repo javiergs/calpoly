d7db72c6bb490efedf9ce71b0a02023f31b638c9
Javier <mscjaviergs@gmail.com>
2022-02-27 04:50:50 -0700

    Update TestDelegate.java

--- src/test/java/TestDelegate.java
+++ src/test/java/TestDelegate.java
@@ -7,8 +7,8 @@ public class TestDelegate{
         @Test
         @DisplayName("Test the answer to everything")
         void testAnswerToAll(){
-            DeepThought superComputer = new DeepThought();
-            assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
+            Delegate superComputer = new Delegate();
+            assertEquals( 42 , superComputer.answer());
         }
 
 }
